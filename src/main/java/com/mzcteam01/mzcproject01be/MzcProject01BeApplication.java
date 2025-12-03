6fc3a3aca1dd53f1b1ffa43a7124b4807844e343
seongje00416 <seongje00416@gmail.com>
2025-12-03 14:29:50 +0900

    Project Initialize

---
+++ src/main/java/com/mzcteam01/mzcproject01be/MzcProject01BeApplication.java
@@ -0,0 +1,13 @@
+package com.mzcteam01.mzcproject01be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MzcProject01BeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MzcProject01BeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mzcteam01/mzcproject01be/MzcProject01BeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mzcteam01.mzcproject01be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MzcProject01BeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
