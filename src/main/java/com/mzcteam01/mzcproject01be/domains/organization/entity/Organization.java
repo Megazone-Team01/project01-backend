f83174906f23d650bff9e842c908b88b2c5f2e35
hye-inA <hye.ina0130@gmail.com>
2025-12-04 12:46:33 +0900

    feat : Organization 엔티티 생성

---
+++ src/main/java/com/mzcteam01/mzcproject01be/domains/organization/entity/Organization.java
@@ -0,0 +1,39 @@
+package com.mzcteam01.mzcproject01be.domains.organization.entity;
+
+import com.mzcteam01.mzcproject01be.domains.user.entity.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "organiazation")
+public class Organization {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false, length = 5)
+    private int id;
+
+    @Column(name = "name", nullable = false, length = 50)
+    private String name;
+
+    @Column(name = "address_code", nullable = false, length = 5)
+    private String addressCode;
+
+    @Lob
+    @Column(name = "address_detail", nullable = false)
+    private String addressDetail;
+
+    @Column(name = "tel", nullable = false, length = 11)
+    private String tel;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @Column(name = "owner_id", nullable = false)
+    private User owner;
+}
