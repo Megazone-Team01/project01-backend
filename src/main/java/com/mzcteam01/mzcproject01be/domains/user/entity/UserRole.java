d3c2e4261aef7ef00a33b3c99b86298cf4ffa1d7
hye-inA <hye.ina0130@gmail.com>
2025-12-04 12:46:14 +0900

    feat : UserRole 엔티티 생성

---
+++ src/main/java/com/mzcteam01/mzcproject01be/domains/user/entity/UserRole.java
@@ -0,0 +1,24 @@
+package com.mzcteam01.mzcproject01be.domains.user.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name ="user_role")
+public class UserRole {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name", nullable = false, length = 50)
+    private String name;
+}
