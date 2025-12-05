815106a2218a939e0ef23f487749c844533711b2
hye-inA <hye.ina0130@gmail.com>
2025-12-05 14:13:27 +0900

    feat : 생성일, 마지막 수정일자 자동기입을 위한 AuditingEntityListener,EntityListener 주석 추가

--- src/main/java/com/mzcteam01/mzcproject01be/common/base/BaseEntity.java
+++ src/main/java/com/mzcteam01/mzcproject01be/common/base/BaseEntity.java
@@ -1,15 +1,19 @@
 package com.mzcteam01.mzcproject01be.common.base;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import java.time.LocalDateTime;
 
 @MappedSuperclass
 @Getter
+@EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
     @CreatedDate
     @Column( name = "created_at" )
