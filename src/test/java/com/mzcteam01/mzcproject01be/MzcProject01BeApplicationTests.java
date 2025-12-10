eb1389357d43fff39494e7e724bf2a3e11e05856
waney <ssdii66@gmail.com>
2025-12-10 12:42:04 +0900

    [#57] fix : main page on/offline 조회 api 구현

--- src/test/java/com/mzcteam01/mzcproject01be/MzcProject01BeApplicationTests.java
+++ src/test/java/com/mzcteam01/mzcproject01be/MzcProject01BeApplicationTests.java
@@ -1,13 +1,29 @@
 package com.mzcteam01.mzcproject01be;
 
+import com.mzcteam01.mzcproject01be.domains.lecture.entity.OnlineLecture;
+import com.mzcteam01.mzcproject01be.domains.lecture.service.HomeService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @SpringBootTest
+@Transactional
 class MzcProject01BeApplicationTests {
 
+    @Autowired
+    HomeService homeService;
+
     @Test
+    @Transactional(readOnly = true)
     void contextLoads() {
+        List<OnlineLecture> list = homeService.getAllOnlineLectures();
+        for (OnlineLecture onlineLecture : list) {
+            System.out.println("title: "+ onlineLecture.getName());
+            System.out.println("description: "+ onlineLecture.getDescription());
+        }
     }
 
 }
