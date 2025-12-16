53e807b74ee50d603bcb49b6462f9c323b412878
hye-inA <hye.ina0130@gmail.com>
2025-12-16 17:23:04 +0900

    feat : 선생님 목록 조회 DTO 생성

---
+++ src/main/java/com/mzcteam01/mzcproject01be/domains/user/dto/response/TeacherListResponse.java
@@ -0,0 +1,31 @@
+package com.mzcteam01.mzcproject01be.domains.user.dto.response;
+
+import com.mzcteam01.mzcproject01be.common.enums.ChannelType;
+import com.mzcteam01.mzcproject01be.domains.user.entity.User;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@Builder
+public class TeacherListResponse {
+
+    private int teacherId;
+    private String name;
+    private int type;
+    private String typeName;
+    private List<String> categories; // 담당 강의 카테고리 목록
+
+    public static TeacherListResponse from(User teacher, List<String> categoryNames) {
+        ChannelType channelType = ChannelType.fromCode(teacher.getType());
+
+        return TeacherListResponse.builder()
+                .teacherId(teacher.getId())
+                .name(teacher.getName())
+                .type(teacher.getType())
+                .typeName(ChannelType.ALL.getDescription())
+                .categories(categoryNames)
+                .build();
+    }
+}
