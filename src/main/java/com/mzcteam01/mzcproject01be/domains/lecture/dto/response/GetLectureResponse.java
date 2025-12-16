36c01a003ff569ff210131a3d00137fe220309fa
waney <ssdii66@gmail.com>
2025-12-16 11:20:24 +0900

    [#98] refactor: lecture DTO response 수강 시자, 종료 포함

--- src/main/java/com/mzcteam01/mzcproject01be/domains/lecture/dto/response/GetLectureResponse.java
+++ src/main/java/com/mzcteam01/mzcproject01be/domains/lecture/dto/response/GetLectureResponse.java
@@ -3,6 +3,8 @@ package com.mzcteam01.mzcproject01be.domains.lecture.dto.response;
 import com.mzcteam01.mzcproject01be.domains.lecture.entity.Lecture;
 import lombok.*;
 
+import java.time.LocalDateTime;
+
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -12,12 +14,16 @@ public class GetLectureResponse {
     private int id;
     private String title;
     private String description;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
 
     public static GetLectureResponse of(Lecture lecture) {
         return GetLectureResponse.builder()
                 .id(lecture.getId())
                 .title(lecture.getName())
                 .description(lecture.getDescription())
+                .startTime(lecture.getStartAt())
+                .endTime(lecture.getEndAt())
                 .build();
     }
 }
