ba601c20dc273087b8c59825c1b1afa8bfc41fbf
hye-inA <hye.ina0130@gmail.com>
2025-12-17 18:34:29 +0900

    feat : 선생님 조회 실패 에러코드 추가

--- src/main/java/com/mzcteam01/mzcproject01be/common/exception/UserErrorCode.java
+++ src/main/java/com/mzcteam01/mzcproject01be/common/exception/UserErrorCode.java
@@ -1,10 +1,11 @@
 package com.mzcteam01.mzcproject01be.common.exception;
-import lombok.AllArgsConstructor;
+
 import lombok.Getter;
 
 @Getter
 public enum UserErrorCode {
     USER_NOT_FOUND( "해당하는 유저를 찾을 수 없습니다" ),
+    TEACHER_NOT_FOUND("해당하는 선생님을 찾을 수 없습니다."),
     PASSWORD_NOT_MATCH("비밀번호가 일치하지 않습니다."),
     EMAIL_ALREADY_EXISTS("이미 존재하는 이메일입니다."),
     EMAIL_NOT_FOUND("존재하지 않는 이메일입니다."),
