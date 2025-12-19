ec7482225af506a086670dcfbdebe41c002b5344
hye-inA <hye.ina0130@gmail.com>
2025-12-19 14:52:34 +0900

    feat : Swagger 태그 추가

--- src/main/java/com/mzcteam01/mzcproject01be/domains/meeting/controller/MeetingController.java
+++ src/main/java/com/mzcteam01/mzcproject01be/domains/meeting/controller/MeetingController.java
@@ -6,6 +6,8 @@ import com.mzcteam01.mzcproject01be.domains.meeting.service.MeetingService;
 import com.mzcteam01.mzcproject01be.domains.user.dto.response.TeacherDetailResponse;
 import com.mzcteam01.mzcproject01be.domains.user.dto.response.TeacherListResponse;
 import com.mzcteam01.mzcproject01be.security.AuthUser;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -16,11 +18,14 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/meetings")
 @RequiredArgsConstructor
+@Tag(name = "Meeting", description = "상담 API")
 public class MeetingController {
 
     private final MeetingService meetingService;
 
     @GetMapping("/teachers")
+    @Operation( summary = "상담 가능한 선생님 목록 조회",
+                description = "organizationId로 기관별 상담 가능한 선생님 목록을 조회함")
     public ResponseEntity<List<TeacherListResponse>> getTeachersList(@RequestParam int organizationId) {
 
         List<TeacherListResponse> teachers = meetingService.getTeachersList(organizationId);
@@ -29,6 +34,8 @@ public class MeetingController {
     }
 
     @GetMapping("/teachers/{teacherId}")
+    @Operation( summary = "선생님 상세 정보 조회",
+                description = "선생님 상세 정보 조회함")
     public ResponseEntity<TeacherDetailResponse> getTeacherDetails(@PathVariable int teacherId) {
 
         TeacherDetailResponse teacherDetail = meetingService.getTeacherDetails(teacherId);
@@ -36,6 +43,8 @@ public class MeetingController {
     }
 
     @GetMapping("/my")
+    @Operation( summary = "내 상담 목록 조회",
+                description = "로그인한 학생의 상담 목록을 조회함")
     public ResponseEntity<List<MyMeetingListResponse>> getMyMeetings(
             @AuthenticationPrincipal AuthUser authUser,
             @RequestParam(defaultValue = "ALL") String type,
