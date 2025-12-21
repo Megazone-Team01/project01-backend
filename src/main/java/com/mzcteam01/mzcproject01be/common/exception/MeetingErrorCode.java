92defe515c30aba05c86584689b2d86de00accaf
hye-inA <hye.ina0130@gmail.com>
2025-12-21 23:26:47 +0900

    feat : 반려 사유 필수 예외처리 에러코드 추가

--- src/main/java/com/mzcteam01/mzcproject01be/common/exception/MeetingErrorCode.java
+++ src/main/java/com/mzcteam01/mzcproject01be/common/exception/MeetingErrorCode.java
@@ -8,7 +8,8 @@ public enum MeetingErrorCode {
     TIME_SLOT_NOT_AVAILABLE("이미 예약된 시간대입니다."),
     LOCATION_REQUIRED("온라인 상담 승인 시 화상회의 링크는 필수입니다."),
     NOT_YOUR_MEETING("본인의 상담만 취소 가능합니다."),
-    CANNOT_CANCEL_APPROVED("승인된 상담은 취소할 수 없습니다. 관리자에게 문의하세요.");
+    CANNOT_CANCEL_APPROVED("승인된 상담은 취소할 수 없습니다. 관리자에게 문의하세요."),
+    REJECT_REASON_REQUIRED("반려 사유는 필수입니다.");
 
 
     private String message;
