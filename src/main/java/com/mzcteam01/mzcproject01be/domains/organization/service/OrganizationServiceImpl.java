e4edfd423b129f9a6ca98e2a958ecee4d5a872ac
Lim Seongje <96779047+seongje00416@users.noreply.github.com>
2025-12-22 21:48:27 +0900

    feat: 알림 기능 추가

--- src/main/java/com/mzcteam01/mzcproject01be/domains/organization/service/OrganizationServiceImpl.java
+++ src/main/java/com/mzcteam01/mzcproject01be/domains/organization/service/OrganizationServiceImpl.java
@@ -27,6 +27,7 @@ import com.mzcteam01.mzcproject01be.domains.user.entity.UserOrganization;
 import com.mzcteam01.mzcproject01be.domains.user.repository.UserOrganizationRepository;
 import com.mzcteam01.mzcproject01be.domains.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -36,6 +37,7 @@ import java.util.List;
 
 @RequiredArgsConstructor
 @Service
+@Slf4j
 public class OrganizationServiceImpl implements OrganizationService{
     private final OrganizationRepository organizationRepository;
     private final QOrganizationRepository qOrganizationRepository;
@@ -178,11 +180,19 @@ public class OrganizationServiceImpl implements OrganizationService{
         // 알람 전달
         // 강사가 신청한 경우 -> 대표에게 알림
         if( user.getRole().getName().equals("TEACHER") ) {
-            String message = String.format( "새로운 강사님이 기관에 등록 신청을 했습니다");
+            String message = String.format( "새로운 강사님이 기관에 등록을 신청을 했습니다");
             notificationService.sendNotification( organization.getOwner().getId(), message );
         }
         // 학생이 신청한 경우 -> 강사들 + 대표에게 알림
-
+        else if( user.getRole().getName().equals("STUDENT") ){
+            List<UserOrganization> organizationTeachers = userOrganizationRepository.findAllByOrganizationId( organizationId ).stream().filter(
+                    uo -> uo.getUser().getRole().getName().equals("TEACHER")
+            ).toList();
+            log.info( "size: {}", organizationTeachers.size() );
+            String message = "새로운 학생이 아카데미에 등록을 신청했습니다.";
+            for( UserOrganization uo : organizationTeachers ) notificationService.sendNotification( uo.getUser().getId(), message );
+            notificationService.sendNotification( organization.getOwner().getId(), message );
+        }
     }
 
     private List<Lecture> getAllOnlineLectureByOrganizationId( int organizationId ) {
